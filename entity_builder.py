712c07df93fc6655f780bf2007f31b8356d3a6fd
unknown <africamultiple-digital@uni-bayreuth.de>
2024-06-05 13:58:01 +0200

    Minor changes entity_builder

--- entity_builder.py
+++ entity_builder.py
@@ -13,7 +13,7 @@ import json
 from wisski.api import Api, Pathbuilder, Entity
 
 # Local Functions
-from functions import entity_uri, json_file
+from functions import entity_uri, json_file, entity_list_generate
 
 # Class for creating the Research Data Item entity
 
@@ -28,14 +28,18 @@ class DocumentEntity:
         self._api_url = "***REMOVED***/wisski/api/v0"
         self._auth = ("***REMOVED***", "***REMOVED***")
         self._api = Api(self._api_url, self._auth, {"Cache-Control": "no-cache"})
-        self._api.pathbuilders = ['amo_ecrm__v01_dev_pb']
+        self._api.pathbuilders = ["amo_ecrm__v01_dev_pb"]
         # Core dictionary for Research Data Items
         self._research_data_item = {
             # Type of Resource (Mandatory Field)
-            self._field['f_research_data_item_type_res']: [entity_uri(self._document.get('typeOfResource'),
+            self._field.get('f_research_data_item_type_res'): [entity_uri(self._document.get('typeOfResource'),
                                                                       self._query.get('typeofresource'))],
             # Field for Identifiers (Mandatory Field)
-            self._bundle['g_research_data_item_identifier']: self.identifier_entities(),
+            self._bundle.get('g_research_data_item_identifier'): self.identifier_entities(),
+            # Sponsorship (Mandatory Field)
+            self._field.get('f_research_data_item_sponsor'): self._document.get('sponsor')
+            # Citation
+
         }
     # Entity list of identifiers
 
@@ -52,3 +56,26 @@ class DocumentEntity:
                                        bundle_id=self._bundle['g_research_data_item_identifier'])
             entity_list.append(identifier_entity)
         return entity_list
+
+    # Language
+    def langauge(self):
+        if not self._document.get('langauge') == []:
+            self._research_data_item[self._field.get('f_research_data_item_language')] = entity_list_generate(
+                self._document.get('language'),
+                self._query.get('language')
+            )
+        else:
+            pass
+
+    # Citation
+    def citation(self):
+        if not self._document.get(citation) == []:
+            self._research_data_item[self._field.get('f_research_data_item_citation')] = self._document.get('citation')
+        else:
+            pass
+
+    # Staged Values
+    def staging(self):
+        self.langauge()
+        self.citation()
+        return self._research_data_item
