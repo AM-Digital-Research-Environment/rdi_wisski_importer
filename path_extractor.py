aa2eb0816768fe01fcd66fee883120c1e5e897a4
unknown <africamultiple-digital@uni-bayreuth.de>
2024-06-14 14:45:11 +0200

    Pathbuilder update

--- path_extractor.py
+++ path_extractor.py
@@ -2,21 +2,23 @@
 import xml.etree.ElementTree as eT
 import json
 
-tree = eT.parse(r'pathbuilder\amo_ecrm__v01_dev_pb_20240604T153133.xml')
-root = tree.getroot()
 
-bundles = {}
-fields = {}
+def pathbuilder_save(xml_file_name):
+    tree = eT.parse(xml_file_name)
+    root = tree.getroot()
 
-for path in root.findall('path'):
-    if path.find('is_group').text == '1':
-        bundles[path.find('id').text] = path.find('bundle').text
-    elif path.find('is_group').text == '0':
-        fields[path.find('id').text] = path.find('field').text
+    bundles = {}
+    fields = {}
 
-# Generating json files
-with open('dicts\\bundles.json', 'w') as bfile:
-    json.dump(bundles, bfile)
+    for path in root.findall('path'):
+        if path.find('is_group').text == '1':
+            bundles[path.find('id').text] = path.find('bundle').text
+        elif path.find('is_group').text == '0':
+            fields[path.find('id').text] = path.find('field').text
 
-with open('dicts\\fields.json', 'w') as ffile:
-    json.dump(fields, ffile)
+    # Generating json files
+    with open('dicts\\bundles.json', 'w') as bfile:
+        json.dump(bundles, bfile)
+
+    with open('dicts\\fields.json', 'w') as ffile:
+        json.dump(fields, ffile)
