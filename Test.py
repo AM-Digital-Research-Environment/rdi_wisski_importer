9193511fd1748c24d042b8aca9c66d3a197ff3b1
neerajamubt <Neeraj.Thandayan-Viswajith@uni-bayreuth.de>
2024-06-17 17:09:42 +0200

    GeoLoc functions added

--- Test.py
+++
@@ -1,9 +0,0 @@
-from functions import mongodata_fetch
-
-data = mongodata_fetch("projects_metadata_ubt","Humanitar2019")
-
-data_1 = data[0]
-
-from entity_builder import DocumentEntity
-
-DocumentEntity(data_1)._research_data_item
