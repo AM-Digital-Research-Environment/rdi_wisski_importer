baec6e9b83e08d87b2b3e44b0057cfd290017894
unknown <africamultiple-digital@uni-bayreuth.de>
2024-06-18 18:00:04 +0200

    Structural changes and Genre Function

--- exception_functions.py
+++ exception_functions.py
@@ -1,6 +1,48 @@
-"""
-@author: AfricaMultiple (Neeraj Viswajith)
-"""
+# Libraries
+from functions import json_file
+from auth import GeneralEntity
 
-# Exception functions for wherein new values b´must be added to the system
-# Expection functions to be written for fields containing controlled vocabs
+# WissKi Api
+from wisski.api import Api, Pathbuilder, Entity
+
+# Exception functions for wherein new values must be added to the system
+# Exception functions to be written for fields containing controlled vocabs
+
+
+class fieldfunction(GeneralEntity):
+
+    def __init__(self, field_name):
+
+        # Super Class
+        super().__init__()
+
+        # Field Dictionary
+        # TODO: Add Genre to Dict
+        self._path_dict = {
+            'language': {
+                'bundle': self._bundle.get('g_iso_language'),
+                'field': self._field.get('f_iso_language_identifier')
+            },
+            'subregion': {
+                'bundle': self._bundle.get('g_subregion'),
+                'field': self._field.get('f_subregion_name'),
+                'qualifier': self._field.get('f_subregion_region')
+            }
+        }
+
+        # Field/Bundle
+        self._field_info = self._path_dict.get(field_name)
+
+    # Generalised Exception Function
+    def exception(self, entity_value, qualifier_value=None, with_qualifier=False):
+        if entity_value is not None:
+            fields_data = {self._field_info.get('field'): [entity_value]}
+            if with_qualifier:
+                fields_data[self._field_info.get('qualifier')] = [qualifier_value]
+            else:
+                pass
+            return Entity(api=self._api,
+                          fields=fields_data,
+                          bundle_id=self._field_info.get('bundle'))
+        else:
+            pass
