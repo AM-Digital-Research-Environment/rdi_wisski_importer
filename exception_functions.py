1eeb738abd9f3a10aa221d23f1440062e26d9b92
neerajamubt <Neeraj.Thandayan-Viswajith@uni-bayreuth.de>
2024-06-18 10:14:23 +0200

    Exception Functions Added

---
+++ exception_functions.py
@@ -0,0 +1,6 @@
+"""
+@author: AfricaMultiple (Neeraj Viswajith)
+"""
+
+# Exception functions for wherein new values b´must be added to the system
+# Expection functions to be written for fields containing controlled vocabs
