a63af8d9ef3e85d717e24017403401c8fb8c2aac
Neeraj Viswajith <Neeraj.Thandayan-Viswajith@uni-bayreuth.de>
2024-09-23 10:04:27 +0200

    Update example.py

--- example.py
+++ example.py
@@ -5,7 +5,7 @@ from wisski.api import Api
 import functions
 from entity_builder import DocumentEntity
 
-api = Api("https://www.wisski.uni-bayreuth.de/wisski/api/v0", auth=("***REMOVED***", "***REMOVED***"), headers={"Cache-Control": "no-cache"})
+api = Api("https://www.wisski.uni-bayreuth.de/wisski/api/v0", auth=("", ""), headers={"Cache-Control": "no-cache"})
 api.pathbuilders = ["amo_ecrm__v01_dev_pb"]
 
 # pathbuilder_save("https://www.wisski.uni-bayreuth.de/sites/default/files/wisski_pathbuilder/export/amo_ecrm__v01_dev_pb_20240821T122919")
