0d10b37077c213b4ede9f34773f18c5e3c964dbf
jiayuy-bay <Jiayu.Yang@uni-bayreuth.de>
2024-09-30 10:53:20 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-
+# 1. Upload data from MongoDB to WissKI
 from tqdm import tqdm
 from wisski.api import Api
 
@@ -11,8 +11,52 @@ api.pathbuilders = ["amo_ecrm__v01_dev_pb"]
 
 # pathbuilder_save("https://www.wisski.uni-bayreuth.de/sites/default/files/wisski_pathbuilder/export/amo_ecrm__v01_dev_pb_20240821T122919")
 data = functions.mongodata_fetch("projects_metadata_ubt", "UBT_DigiRet2022")
-cache: dict[str, str] = {}
-for row in tqdm(data[:100]):
-    staged = DocumentEntity(row, api).staging()
 
-    uploaded = DocumentEntity(row, api).upload()
+# Run as a loop
+for row in tqdm(data[:100]):
+    doc = DocumentEntity(api=api, bson_doc=row)
+    doc.staging()
+    doc.upload()
+
+
+    
+# 2. WissKi entity sync-up
+from wisski.api import Api
+import entity_sync
+from entity_sync import EntitySync
+
+# Sync-up 'institutions' as an example (always update institutions before persons)
+institutions = EntitySync(api=api, sync_field='institutions')
+
+# For list of missing entities 
+institutions.missing_entities()
+ 
+# For staged Data
+institutions.staged()
+ 
+# For uploading
+institutions.update()
+
+
+
+# 3. WissKi entity Updater
+from wisski.api import Api
+import functions
+import entity_updater
+from entity_updater import DocumentUpdate
+from entity_builder import DocumentEntity
+
+# Fill in the WissKI API username and password below
+api = Api("https://www.wisski.uni-bayreuth.de/wisski/api/v0", auth=("username", "password"), headers={"Cache-Control": "no-cache"})
+api.pathbuilders = ["amo_ecrm__v01_dev_pb"]
+
+# Example
+updater = DocumentUpdate(
+api=api,
+dre_identifier="aca-01-0000",
+method='subject' or ['citation', 'language', 'subject'],
+mongodb="projects_metadata_ubt",
+mongocoll="UBT_DigiRet2022"
+)
+
+updater.run()
